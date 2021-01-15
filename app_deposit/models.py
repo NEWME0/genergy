ce2c195dfd9d10a0bd748333074be32aafa3c4fd
artiom <ordersone@gmail.com>
2021-01-15 16:33:18 +0200

    Initialize models

--- app_deposit/models.py
+++ app_deposit/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+from django.db.models import *
+from django.core.exceptions import ValidationError
+from polymorphic.models import PolymorphicModel
 
-# Create your models here.
+
+class Entry(PolymorphicModel):
+    """ Обьекты склада. """
+
+    title = CharField(max_length=255, unique=True)
+
+
+class Stock(Model):
+    """ Обьекты на складе. """
+
+    entry = ForeignKey(to=Entry, on_delete=PROTECT)
+    count = PositiveIntegerField()
+    price = FloatField()
+
+
+class Instrument(Entry):
+    """ Интструменты. Тип обьектов склада. """
+
+
+class Consumable(Entry):
+    """ Расходники (материалы). Тип обьектов склада. """
--- app_service/models.py
+++ app_service/models.py
@@ -1,3 +1,28 @@
-from django.db import models
+from django.db.models import *
 
-# Create your models here.
+from app_deposit.models import Consumable, Instrument
+
+
+class ServiceConsumable(Model):
+    """ Расходники (материалы) используемые для выполнения одной работы. """
+
+    consumable = ForeignKey(to=Consumable, on_delete=CASCADE)
+    service = ForeignKey(to='Service', on_delete=CASCADE)
+    count = PositiveIntegerField()
+
+
+class ServiceInstrument(Model):
+    """ Интсрументы используемые при выполнении одной работы. """
+
+    instrument = ForeignKey(to=Instrument, on_delete=CASCADE)
+    service = ForeignKey(to='Service', on_delete=CASCADE)
+    count = PositiveIntegerField()
+
+
+class Service(Model):
+    """ Тип работ. """
+
+    title = CharField(max_length=255, unique=True)
+
+    consumables = ManyToManyField(to=Consumable, through=ServiceConsumable, through_fields=['service', 'consumable'])
+    instruments = ManyToManyField(to=Instrument, through=ServiceInstrument, through_fields=['service', 'instrument'])
