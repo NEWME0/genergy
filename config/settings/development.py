65992add4bbb7d9eb6ae0bc3af7b31b736fc12ab
artiom <ordersone@gmail.com>
2021-01-18 18:06:20 +0200

    Fix settings.development

--- config/settings/development.py
+++ config/settings/development.py
@@ -4,18 +4,4 @@ from .base import *
 
 
 DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1']
-
-
-# Swagger settings
-if 'drf_spectacular' in INSTALLED_APPS:
-    pass
-
-
-# Debug toolbar settings
-if 'debug_toolbar' in INSTALLED_APPS:
-    MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
-
-    DEBUG_TOOLBAR_CONFIG = {
-        'INTERCEPT_REDIRECTS': False,
-    }
+ALLOWED_HOSTS = ['*']
