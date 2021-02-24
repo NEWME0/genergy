38202f3bd5ec08946338cd7e609e8b2478d97c16
Rotari Artiom <ordersone@gmail.com>
2021-02-24 22:46:17 +0200

    Added soft deletion for models and managers

---
+++ common/manages.py
@@ -0,0 +1,26 @@
+
+from django.db.models import QuerySet, Manager
+from django.utils import timezone
+
+
+class BaseQuerySet(QuerySet):    
+    def delete(self):
+        return super(BaseQuerySet, self).update(deleted_at=timezone.now())
+
+    def hard_delete(self):
+        return super(BaseQuerySet, self).delete()
+
+
+class BaseManager(Manager):
+    def __init__(self, *args, with_deleted=False, **kwargs):
+        super(BaseManager, self).__init__(*args, **kwargs)
+        self.with_deleted = with_deleted
+
+    def get_queryset(self):
+        if self.with_deleted:
+            return BaseQuerySet(self.model)
+        else:
+            return BaseQuerySet(self.model).filter(deleted_at__isnull=True)
+
+    def hard_delete(self):
+        return self.get_queryset().hard_delete()
--- common/models.py
+++ common/models.py
@@ -1,9 +1,23 @@
-from django.db.models import *
+from django.db.models import Model, DateTimeField
+from django.utils import timezone
+
+from common.manages import BaseManager
 
 
 class BaseModel(Model):
+    all_objects = BaseManager(with_deleted=True)
+    objects = BaseManager(with_deleted=False)
+
     created_at = DateTimeField(auto_now_add=True)
     updated_at = DateTimeField(auto_now=True)
+    deleted_at = DateTimeField(null=True)
 
     class Meta:
         abstract = True
+
+    def delete(self, using=None, keep_parents=False):
+        self.deleted_at = timezone.now()
+        self.save()
+
+    def hard_delete(self, using=None, keep_parents=False):
+        return super(BaseModel, self).delete(using=using, keep_parents=keep_parents)
