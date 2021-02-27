04746344b77c080f3d42dc7d6d07da66d140c8c8
Rotari Artiom <ordersone@gmail.com>
2021-02-27 21:44:15 +0200

    Added supply and afford methods for Items and Utils

--- app_entities/models.py
+++ app_entities/models.py
@@ -1,26 +1,14 @@
-from django.db.models import CharField, FloatField, CheckConstraint, Q
+from django.contrib.auth import get_user_model
+from django.db import transaction
+from django.db.models import CharField, FloatField, ForeignKey, PositiveIntegerField
+from django.db.models.deletion import PROTECT
+from django.db.models.query_utils import Q
+from django.db.models.constraints import CheckConstraint, UniqueConstraint
 
 from common.models import BaseModel
 
 
-class Item(BaseModel):
-    title = CharField(max_length=255)
-    price = FloatField(default=0)
-
-    class Meta:
-        constraints = [
-            CheckConstraint(name='item_price', check=Q(price__gte=0))
-        ]
-
-
-class Util(BaseModel):
-    title = CharField(max_length=255)
-    price = FloatField(default=0)
-
-    class Meta:
-        constraints = [
-            CheckConstraint(name='util_price', check=Q(price__gte=0))
-        ]
+User = get_user_model()
 
 
 class Work(BaseModel):
@@ -30,4 +18,74 @@ class Work(BaseModel):
     class Meta:
         constraints = [
             CheckConstraint(name='work_price', check=Q(price__gte=0))
+        ]
+
+
+class Item(BaseModel):
+    title = CharField(max_length=255)
+    price = FloatField(default=0)
+    count = PositiveIntegerField(default=0)
+
+    class Meta:
+        constraints = [
+            CheckConstraint(name='item_price', check=Q(price__gte=0))
+        ]
+
+    def supply(self, count: int):
+        self.count += count
+        self.save()
+
+    def afford(self, count: int, user: User):
+        user_item, created = UserItem.objects.get_or_create(user=user, item=self)
+        user_item.count += count
+        self.count -= count
+
+        with transaction.atomic():
+            user_item.save()
+            self.save()
+
+
+class Util(BaseModel):
+    title = CharField(max_length=255)
+    price = FloatField(default=0)
+    count = PositiveIntegerField(default=0)
+
+    class Meta:
+        constraints = [
+            CheckConstraint(name='util_price', check=Q(price__gte=0))
+        ]
+
+    def supply(self, count: int):
+        self.count += count
+        self.save()
+
+    def afford(self, count: int, user: User):
+        user_item, created = UserUtil.objects.get_or_create(user=user, util=self)
+        user_item.count += count
+        self.count -= count
+
+        with transaction.atomic():
+            user_item.save()
+            self.save()
+
+
+class UserItem(BaseModel):
+    item = ForeignKey(to=Item, on_delete=PROTECT, related_name='in_use')
+    user = ForeignKey(to=User, on_delete=PROTECT, related_name='items')
+    count = PositiveIntegerField(default=0)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(name='unique_user_item', fields=['item', 'user'])
+        ]
+
+
+class UserUtil(BaseModel):
+    util = ForeignKey(to=Util, on_delete=PROTECT, related_name='in_use')
+    user = ForeignKey(to=User, on_delete=PROTECT, related_name='utils')
+    count = PositiveIntegerField(default=0)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(name='unique_user_util', fields=['util', 'user'])
         ]
--- app_entities/serializers.py
+++ app_entities/serializers.py
@@ -1,23 +1,9 @@
-from rest_framework.fields import FloatField
-from rest_framework.serializers import ModelSerializer
+from django.contrib.auth import get_user_model
+from rest_framework.fields import FloatField, IntegerField
+from rest_framework.relations import PrimaryKeyRelatedField
+from rest_framework.serializers import ModelSerializer, Serializer
 
 from app_entities.models import Item, Util, Work
-
-
-class ItemSerializer(ModelSerializer):
-    price = FloatField(min_value=0)
-
-    class Meta:
-        model = Item
-        fields = ['id', 'title', 'price']
-
-
-class UtilSerializer(ModelSerializer):
-    price = FloatField(min_value=0)
-
-    class Meta:
-        model = Util
-        fields = ['id', 'title', 'price']
 
 
 class WorkSerializer(ModelSerializer):
@@ -26,3 +12,40 @@ class WorkSerializer(ModelSerializer):
     class Meta:
         model = Work
         fields = ['id', 'title', 'price']
+
+
+class ItemSerializer(ModelSerializer):
+    price = FloatField(min_value=0)
+
+    class Meta:
+        model = Item
+        fields = ['id', 'title', 'price', 'count']
+
+
+class UtilSerializer(ModelSerializer):
+    price = FloatField(min_value=0)
+
+    class Meta:
+        model = Util
+        fields = ['id', 'title', 'price', 'count']
+
+
+class SupplySerializer(Serializer):
+    count = IntegerField(min_value=0)
+
+    def create(self, validated_data):
+        raise NotImplementedError()
+
+    def update(self, instance, validated_data):
+        raise NotImplementedError()
+
+
+class GivingSerializer(Serializer):
+    count = IntegerField(min_value=0)
+    user = PrimaryKeyRelatedField(queryset=get_user_model().objects.filter(is_agent_account=True))
+
+    def create(self, validated_data):
+        raise NotImplementedError()
+
+    def update(self, instance, validated_data):
+        raise NotImplementedError()
